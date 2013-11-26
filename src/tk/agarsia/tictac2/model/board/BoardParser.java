a0b52684fa4a63187630cd99ccc4e81d6e3a4509
agarsia <agarsianHero@gmail.com>
2013-11-26 22:44:24 +0100

    performance optimization in BoardParser (still having unsolved bug)

--- src/tk/agarsia/tictac2/model/board/BoardParser.java
+++ src/tk/agarsia/tictac2/model/board/BoardParser.java
@@ -1,11 +1,11 @@
 package tk.agarsia.tictac2.model.board;
 
 public class BoardParser {
-	public static int testBoardForWinner(int[] arr, int len, int wLen) {
+	public static int testBoardForWinner9(int[] arr, int len, int wLen) {
 		int count = 0;
 		
 		for (int x = 0; x < len; x++) {
-			// horizontal testing 
+			// horizontal testing
 			for (int y = 0; y < len - (wLen - 1); y++) {
 				int tf = arr[y + x * len];
 				System.out.println("(" + x + "," + y + ") " + tf);
@@ -20,28 +20,7 @@ public class BoardParser {
 							break;
 					}
 				}
-				
-				//TODO merge horizontal & backslash (should work (99%))
-				
-				// diagonals backslash
-				if (x < len - (wLen - 1)) {
-					System.out.println("(" + x + "," + y + ") " + tf);
-					if (tf != 0) {
-						for (int z = 1; z <= wLen; z++) {
-							count++;
-							System.out.println("- (" + x + "," + y + "," + z
-									+ ") " + tf);
-							if (wLen == z)
-								return tf;
-							else if (wLen > z
-									&& arr[y + (x + z) * len + z] != tf)
-								break;
-						}
-					}
-				}
 			}
-			
-			//TODO merge all?? (50%)
 
 			if (x < len - (wLen - 1)) {
 				// vertical testing
@@ -61,9 +40,25 @@ public class BoardParser {
 					}
 				}
 
-				//TODO merge vertical & slash (should work too (60%))
-				
-				// diagonals slash
+				// diagonals top left
+				for (int y = 0; y < len - (wLen - 1); y++) {
+					int tf = arr[y + x * len];
+					System.out.println("(" + x + "," + y + ") " + tf);
+					if (tf != 0) {
+						for (int z = 1; z <= wLen; z++) {
+							count++;
+							System.out.println("- (" + x + "," + y + "," + z
+									+ ") " + tf);
+							if (wLen == z)
+								return tf;
+							else if (wLen > z
+									&& arr[y + (x + z) * len + z] != tf)
+								break;
+						}
+					}
+				}
+
+				// diagonals top right
 				for (int y = len - 1; y > len - wLen; y--) {
 					int tf = arr[y + x * len];
 					System.out.println("(" + x + "," + y + ") " + tf);
@@ -87,6 +82,153 @@ public class BoardParser {
 
 		return 0;
 	}
+	
+	public static int testBoardForWinner5(int[] arr, int len, int wLen) {
+		int count = 0;
+		int tfh,tfb,tfv,tfs;
+		
+		for (int x = 0; x < len; x++) {
+			for (int y = 0; y < len - (wLen - 1); y++) {
+				tfh = arr[y + x * len]; //tfh for horizontals
+				tfb = arr[y + x * len]; //tfb for backslash
+				
+				if (tfh != 0) { //abort if field are free (tfh==tfb==0)
+					for (int z = 1; z <= wLen; z++) {
+						System.out.println("- (" + x + "," + y + "," + z + ") "
+								+ tfh+ " - "+tfb);
+						
+						if(tfh!=0) //horizontal
+							if (wLen == z)
+								return tfh;
+							else if (wLen > z && arr[y + x * len + z] != tfh) {
+								count++;
+								tfh = 0;
+							}
+						
+						if (x < len - (wLen - 1)&&tfb!=0) //backslash
+							if (wLen == z)
+								return tfb;
+							else if (wLen > z
+									&& arr[y + (x + z) * len + z] != tfb) {
+								count++;
+								tfb = 0;
+							}
+					}
+				}
+			}
+			
+			//TODO merge all?? (50%)
+
+			if (x < len - (wLen - 1)) {
+				// vertical testing
+				for (int y = 0; y < len; y++) {
+					tfv = arr[y + x * len]; //for vertical
+					tfs = arr[y + x * len]; //for slash
+					
+					if (tfv != 0) { //free field no need to test (tfv==tfs==0)
+						for (int z = 1; z <= wLen; z++) {
+							System.out.println("- (" + x + "," + y + "," + z
+									+ ") " + tfv);
+							
+							//vertical
+							if(tfv!=0)
+								if (wLen == z)
+									return tfv;
+								else if (wLen > z && arr[y + (x + z) * len] != tfv) {
+									count++;
+									tfv=0;
+								}
+									
+							//slash
+							if(y>len-wLen&&y<len&&tfs!=0)
+								if (wLen == z)
+									return tfs;
+								else if (wLen > z
+										&& arr[y + (x + z) * len - z] != tfs) {
+									count++;
+									tfs=0;
+								}
+						}
+					}
+				}
+
+			}
+		}
+
+		System.out.println("------"+count);
+
+		return 0;
+	}
+	
+	public static int testBoardForWinner(int[] arr, int len, int wLen) {
+		int count = 0;
+		int tfh,tfb,tfv,tfs;
+		
+		for (int x = 0; x < len; x++) {
+			for (int y = 0; y < len; y++) {
+					tfv = arr[y + x * len]; //tfv for vertical
+					tfs = arr[y + x * len]; //tfs for slash
+					tfh = arr[y + x * len]; //tfh for horizontals
+					tfb = arr[y + x * len]; //tfb for backslash
+					
+					if (y < len - (wLen - 1)&&tfh != 0) { //abort if field are free (tfh==tfb==0)
+						for (int z = 1; z <= wLen; z++) {
+							System.out.println("- (" + x + "," + y + "," + z + ") "
+									+ tfh+ " - "+tfb);
+							
+							if(tfh!=0) //horizontal
+								if (wLen == z)
+									return tfh;
+								else if (wLen > z && arr[y + x * len + z] != tfh) {
+									count++;
+									tfh = 0;
+								}
+							
+							if (x < len - (wLen - 1)&&tfb!=0) //backslash
+								if (wLen == z)
+									return tfb;
+								else if (wLen > z
+										&& arr[y + (x + z) * len + z] != tfb) {
+									count++;
+									tfb = 0;
+								}
+						}
+					
+					if (x < len - (wLen - 1)&&tfv != 0) { //free field no need to test (tfv==tfs==0)
+						for (int z = 1; z <= wLen; z++) {
+							System.out.println("- (" + x + "," + y + "," + z
+									+ ") " + tfv);
+							
+							//vertical
+							if(tfv!=0)
+								if (wLen == z)
+									return tfv;
+								else if (wLen > z && arr[y + (x + z) * len] != tfv) {
+									count++;
+									tfv=0;
+								}
+									
+							//slash
+							if(y>len-wLen&&y<len&&tfs!=0)
+								if (wLen == z)
+									return tfs;
+								else if (wLen > z
+										&& arr[y + (x + z) * len - z] != tfs) {
+									count++;
+									tfs=0;
+								}
+						}
+					}
+				}
+
+			}
+		}
+
+		System.out.println("------"+count);
+
+		return 0;
+	}
+
 	
 	public static void main(String...args) {
 		int[] arr = new int[] {
@@ -115,7 +257,6 @@ public class BoardParser {
 				1,1,2,2,1,1
 		};
 
-		
 		System.out.println(testBoardForWinner(arr,4,3));
 
 		System.out.println("\n\n");
@@ -124,6 +265,8 @@ public class BoardParser {
 
 		System.out.println("\n\n");
 		
+		long start = System.nanoTime();
 		System.out.println(testBoardForWinner(arr3,6,3));
+		System.out.println((System.nanoTime()-start)+"ns");
 	}
 }
